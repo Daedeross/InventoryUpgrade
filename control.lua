ed2e75bb5fbaf487c2dc537ae92be07c096bc341
Bryan C. Jones <jone0751@umn.edu>
2015-05-21 20:40:53 -0500

    Added script to modify new player. Need to test with multiplayer.

--- control.lua
+++ control.lua
@@ -1,6 +1,8 @@
 require ("defines")
 require("config")
 require("scripts/onInventoryIncrease")
+require("scripts/createNewCharacter")
+
 --require ("util")
 --[[
 game.oninit(function ()
@@ -20,5 +22,29 @@ game.onevent(defines.events.onresearchfinished, function(event)
     end
     if u_level > 0 then
         onInventoryIncrease.main(event, u_level)
+    end 
+end)
+
+game.onevent(defines.events.onplayercreated, function(event)
+    local player_index = event.playerindex
+    local player = game.players[playerindex]
+    if player ~= nil then
+        local u_level = 0
+        local tech_prefix = "inventory-upgrade-"
+        
+        
+        for i=1, MaxInventoryUpgrades do
+            local tech_name = tech_prefix .. i
+            if player.force.technologies[tech_name].researched then
+                index  = i
+            end
+        end
+        
+        if i > 0 then
+            local proto_name = string.format("%s%i", "player-", u_level)
+            local new_proto = game.entityprototypes[proto_name]
+            
+            createNewCharacter.main(player, new_proto)
+        end 
     end 
 end)
